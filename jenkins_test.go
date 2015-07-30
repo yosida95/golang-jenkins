a0e647060d921d988682b5d344326a5aadf77bd2
dennis.leon <dennis.leon39@gmail.com>
2015-07-30 13:49:02 -0700

    Spin up a local jenkins container for tests to run against

--- jenkins_test.go
+++ jenkins_test.go
@@ -2,23 +2,19 @@ package gojenkins
 
 import (
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func NewJenkinsWithTestData() *Jenkins {
-	var auth Auth
-	return NewJenkins(&auth, "http://example.com")
-}
+func Test(t *testing.T) { TestingT(t) }
 
-func Test(t *testing.T) {
+var _ = Suite(&DockerSuite{})
+
+func (s *DockerSuite) TestGettingJobs(c *C) {
 	jenkins := NewJenkinsWithTestData()
 
 	jobs, err := jenkins.GetJobs()
 
-	if err != nil {
-		t.Errorf("error %v\n", err)
-	}
-
-	if len(jobs) == 0 {
-		t.Errorf("return no jobs\n")
-	}
+	c.Assert(err, IsNil)
+	c.Assert(len(jobs), Not(Equals), 0)
 }
