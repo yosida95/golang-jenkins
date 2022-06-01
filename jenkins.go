1a7ff21364ce392a6c213d5f53d68fea0842a0d7
mwatkinson <mwatkinson@newrelic.com>
2022-06-01 14:36:18 -0400

    Revert third party update to check for 200 status code in sendRequest
    This change was made in https://github.com/yosida95/golang-jenkins/commit/3b5bfc05f87c3719955cae4771ce1db2b093c7ea

--- jenkins.go
+++ jenkins.go
@@ -14,16 +14,6 @@ import (
 	"strings"
 )
 
-type HTTPStatusError struct {
-	URL    string
-	Code   int
-	Status string
-}
-
-func (e *HTTPStatusError) Error() string {
-	return fmt.Sprintf("bad http status: %d: %s", e.Code, e.Status)
-}
-
 type Auth struct {
 	Username string
 	ApiToken string
@@ -99,19 +89,7 @@ func (jenkins *Jenkins) sendRequest(req *http.Request) (*http.Response, error) {
 	if jenkins.auth != nil {
 		req.SetBasicAuth(jenkins.auth.Username, jenkins.auth.ApiToken)
 	}
-	res, err := jenkins.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if res.StatusCode >= http.StatusBadRequest {
-		return nil, &HTTPStatusError{
-			URL:    req.URL.String(),
-			Code:   res.StatusCode,
-			Status: res.Status,
-		}
-	}
-	return res, nil
+	return jenkins.client.Do(req)
 }
 
 func (jenkins *Jenkins) parseXmlResponse(resp *http.Response, body interface{}) (err error) {
